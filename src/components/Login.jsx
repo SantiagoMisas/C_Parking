f4a0b39c7d38c820281f5b9eddfc0ef638c3995a
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2022-12-03 11:23:42 -0500

    Se empiezan diseos de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,31 @@
 
 const Login= ()=>{
     return(
-    <section>
-        <form action='' method='POST'>
-            <input value={nombre} placeholder='Nombre' type="text" onChange={(e)=> setNombre(e.target.value)}/>
-            <input value={correo} placeholder='Correo' type="text" onChange={(e)=> setCorreo(e.target.value)}/>
-            <input value={telefono} placeholder='Telefono' type="text" onChange={(e)=> setTelefono(e.target.value)}/>
+    <section className="column">
+
+            <section className="flex">
+                <p className="login_p_title_c">C</p>
+                <p className="login_p_subtitle_">-</p>
+                <p className="login_p_subtitle">Parkin</p>
+                <p className="login_p_subtitle_g">g</p>
+            </section>
+        <form className="form_login" action='' method=''>
+            <input className="login_input" placeholder='Nombre' type="text"/>
+            <input className="login_input" placeholder='Correo' type="text"/>
+            <section className="flex login_section_buttons">
+                <section className="column">
+                    <button className="login_button">Iniciar Sesión</button>
+                    <p className="login_p">¿Ya tienes una cuenta?</p>
+                </section>
+                <section className="column">
+                        <button className="login_button">Registrarse</button>
+                        <p className="login_p">¿No tienes una cuenta?</p>
+                </section>
+            </section>
         </form>
     </section>
     
     )
     }
     
-    export default {Login}
+    export {Login}

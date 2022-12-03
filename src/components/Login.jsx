d8e00e890604c182e1bf9f3dc3121a58b1d15cba
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2022-12-03 13:12:34 -0500

    Retoques de propuesta Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,20 +4,20 @@ const Login= ()=>{
     <section className="column">
 
         <form className="form_login" action='' method=''>
-        <section className="flex">
-                <p className="login_p_title_c">C</p>
-                <p className="login_p_subtitle_">-</p>
-                <p className="login_p_subtitle">Parkin</p>
-                <p className="login_p_subtitle_g">g</p>
-            </section>
         <section className="flex login_section_icons">
                 <img className="login_icon" src="./motorcycle.svg" alt="" />
                 <img className="login_icon" src="./van.svg" alt="" />
                 <img className="login_icon" src="./truck.svg" alt="" />
                 <img className="login_icon" src="./car.svg" alt="" />
             </section>
-            <input className="login_input" placeholder='Nombre' type="text"/>
-            <input className="login_input" placeholder='Correo' type="text"/>
+        <section className="flex login_title">
+                <p className="login_p_title_c">C</p>
+                <p className="login_p_subtitle_">-</p>
+                <p className="login_p_subtitle">Parkin</p>
+                <p className="login_p_subtitle_g">g</p>
+            </section>
+            <input className="login_input" placeholder='Usuario' type="text"/>
+            <input className="login_input" placeholder='Contraseña' type="text"/>
             <section className="flex login_section_buttons">
                 <section className="column">
                     <button className="login_button">Iniciar Sesión</button>

4da474c498aa1ccfc0017bcea6190ed08802044f
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2022-12-03 12:30:53 -0500

    Se anaden iconos a la propuesta login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,13 +3,19 @@ const Login= ()=>{
     return(
     <section className="column">
 
-            <section className="flex">
+        <form className="form_login" action='' method=''>
+        <section className="flex">
                 <p className="login_p_title_c">C</p>
                 <p className="login_p_subtitle_">-</p>
                 <p className="login_p_subtitle">Parkin</p>
                 <p className="login_p_subtitle_g">g</p>
             </section>
-        <form className="form_login" action='' method=''>
+        <section className="flex login_section_icons">
+                <img className="login_icon" src="./motorcycle.svg" alt="" />
+                <img className="login_icon" src="./van.svg" alt="" />
+                <img className="login_icon" src="./truck.svg" alt="" />
+                <img className="login_icon" src="./car.svg" alt="" />
+            </section>
             <input className="login_input" placeholder='Nombre' type="text"/>
             <input className="login_input" placeholder='Correo' type="text"/>
             <section className="flex login_section_buttons">

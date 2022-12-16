40b05583d16e7cca99ff22e497b3e161f473e5a2
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2022-12-16 01:09:50 -0500

    Se añade listar y agregar

--- src/components/TableClient.jsx
+++ src/components/TableClient.jsx
@@ -4,6 +4,22 @@ import { useEffect, useState } from "react";
 const TableClient = () => {
   const uriClientes = "http://localhost:3100/clientes";
   const [clientes, setCliente] = useState([]);
+  const [cedula, setcedula] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [direccion, setDireccion] = useState('');
+  const [celular, setCelular] = useState('');
+  const agregarCliente = async (e) => {
+    //e.preventDefault()//Buscar la forma que guarde y muestre el registro sin recargar la pagina
+
+    const res = await axios.post(uriClientes, {
+      "cedula": cedula,
+      "nombre": nombre,
+      "correo": correo,
+      "direccion": direccion,
+      "celular": celular
+    }
+    )}
   useEffect(() => {
     getClientes();
   }, []);
@@ -14,16 +30,21 @@ const TableClient = () => {
 
   return (
     <section>
-      <section className="flex_wrap">
-        <input className="form_table_input" placeholder="Cedula" type="text" />
-        <input className="form_table_input" placeholder="Nombre" type="text" />
+      <form onSubmit={agregarCliente} className="flex_wrap">
+        <input className="form_table_input" placeholder="Cedula" type="text" value={cedula}
+              onChange={(e)=> setcedula(e.target.value)}/>
+        <input className="form_table_input" placeholder="Nombre" type="text" value={nombre}
+              onChange={(e)=> setNombre(e.target.value)}/>
         <input
           className="form_table_input"
           placeholder="Dirección"
           type="text"
-        />
-        <input className="form_table_input" placeholder="Correo" type="text" />
-        <input className="form_table_input" placeholder="Celular" type="text" />
+		  value={direccion}
+		  onChange={(e)=> setDireccion(e.target.value)}/>
+        <input className="form_table_input" placeholder="Correo" type="text" value={correo}
+              onChange={(e)=> setCorreo(e.target.value)}/>
+        <input className="form_table_input" placeholder="Celular" type="text" value={celular}
+              onChange={(e)=> setCelular(e.target.value)}/>
         <section className="flex_wrap standar_measure">
           <button className="form_table_button">
             <span>Agregar</span>
@@ -38,7 +59,7 @@ const TableClient = () => {
             <span>Eliminar</span>
           </button>
         </section>
-      </section>
+      </form>
 
       <section className="Section_Table">
         <table class="Table">

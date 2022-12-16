c1f7608778f72cfe5b082f043bbeedec4dd32732
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2022-12-16 11:45:57 -0500

    Se arreglo el error de comunicacion con el back de el front

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {Login} from './components/Login';
-import { Navbar } from './components/Navbar';
-import { TableClient } from './components/TableClient';
-import { TableVehicle } from './components/TableVehicle';
-import { TableProduct } from './components/TableProduct';
-import { TableService } from './components/TableService';
-import { TableBilling } from './components/TableBilling';
-import { Footer } from './components/Footer';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Login } from "./components/Login";
+import { Navbar } from "./components/Navbar";
+import TableClient from "./components/TableClient";
+import { TableVehicle } from "./components/TableVehicle";
+import { TableProduct } from "./components/TableProduct";
+import { TableService } from "./components/TableService";
+import { TableBilling } from "./components/TableBilling";
+import { Footer } from "./components/Footer";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <section className="body">
-    <Navbar/>
-   <TableClient/>
-  <Footer/>
+      <Navbar />
+      <TableClient />
+      <Footer />
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TableClient.jsx
+++ src/components/TableClient.jsx
@@ -4,22 +4,22 @@ import { useEffect, useState } from "react";
 const TableClient = () => {
   const uriClientes = "http://localhost:3100/clientes";
   const [clientes, setCliente] = useState([]);
-  const [cedula, setcedula] = useState('');
-  const [nombre, setNombre] = useState('');
-  const [correo, setCorreo] = useState('');
-  const [direccion, setDireccion] = useState('');
-  const [celular, setCelular] = useState('');
+  const [cedula, setcedula] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [celular, setCelular] = useState("");
   const agregarCliente = async (e) => {
     //e.preventDefault()//Buscar la forma que guarde y muestre el registro sin recargar la pagina
 
     const res = await axios.post(uriClientes, {
-      "cedula": cedula,
-      "nombre": nombre,
-      "correo": correo,
-      "direccion": direccion,
-      "celular": celular
-    }
-    )}
+      cedula: cedula,
+      nombre: nombre,
+      correo: correo,
+      direccion: direccion,
+      celular: celular,
+    });
+  };
   useEffect(() => {
     getClientes();
   }, []);
@@ -31,20 +31,41 @@ const TableClient = () => {
   return (
     <section>
       <form onSubmit={agregarCliente} className="flex_wrap">
-        <input className="form_table_input" placeholder="Cedula" type="text" value={cedula}
-              onChange={(e)=> setcedula(e.target.value)}/>
-        <input className="form_table_input" placeholder="Nombre" type="text" value={nombre}
-              onChange={(e)=> setNombre(e.target.value)}/>
+        <input
+          className="form_table_input"
+          placeholder="Cedula"
+          type="text"
+          value={cedula}
+          onChange={(e) => setcedula(e.target.value)}
+        />
+        <input
+          className="form_table_input"
+          placeholder="Nombre"
+          type="text"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
         <input
           className="form_table_input"
           placeholder="Dirección"
           type="text"
-		  value={direccion}
-		  onChange={(e)=> setDireccion(e.target.value)}/>
-        <input className="form_table_input" placeholder="Correo" type="text" value={correo}
-              onChange={(e)=> setCorreo(e.target.value)}/>
-        <input className="form_table_input" placeholder="Celular" type="text" value={celular}
-              onChange={(e)=> setCelular(e.target.value)}/>
+          value={direccion}
+          onChange={(e) => setDireccion(e.target.value)}
+        />
+        <input
+          className="form_table_input"
+          placeholder="Correo"
+          type="text"
+          value={correo}
+          onChange={(e) => setCorreo(e.target.value)}
+        />
+        <input
+          className="form_table_input"
+          placeholder="Celular"
+          type="text"
+          value={celular}
+          onChange={(e) => setCelular(e.target.value)}
+        />
         <section className="flex_wrap standar_measure">
           <button className="form_table_button">
             <span>Agregar</span>
@@ -74,8 +95,8 @@ const TableClient = () => {
             </tr>
           </thead>
           <tbody>
-            {clientes.map((cliente) => (
-              <tr key={cliente.id}>
+            {clientes.map((cliente)=> (
+              <tr>
                 <td>{cliente.cedula}</td>
                 <td>{cliente.nombre}</td>
                 <td>{cliente.correo}</td>
@@ -98,4 +119,4 @@ const TableClient = () => {
   );
 };
 
-export { TableClient };
+export default TableClient;
